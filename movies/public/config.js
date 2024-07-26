3a197b6ab6d1685b4bccae0a888d302d45c95100
sawa-puki <puki.sara.ante@gmail.com>
2024-07-26 14:35:49 +0200

    fix

---
+++ movies/public/config.js
@@ -0,0 +1,22 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://dazzling-licorice-158576.netlify.app/",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmVlOWY1ZjllMDVjYjY2MmIwMTE5NmUxM2YwODQyMyIsIm5iZiI6MTcyMTk5MjI1MS40NzA1NDIsInN1YiI6IjY2YTJmMDU0MGZjODMyMDJjMzQ2ODZjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MV6CMLfwWiCTxyeKwfpRDO32NzLKM_FMefuVLWpkH94",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: true,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: "https://backend.sudo-flix.lol",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+
+  VITE_ALLOW_AUTOPLAY: true,
+};
